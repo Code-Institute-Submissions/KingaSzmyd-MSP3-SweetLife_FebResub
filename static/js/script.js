04f9ed6747e23565561ec6b06b99949b6e85e982
KingaSzmyd <kingaszmyd@gmail.com>
2021-10-07 11:45:29 +0000

    Adding style to flash message in contact form

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,9 @@ var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
 var collapseList = collapseElementList.map(function (collapseEl) {
   return new bootstrap.Collapse(collapseEl)
 })
+
+
+var alertList = document.querySelectorAll('.alert')
+var alerts =  [].slice.call(alertList).map(function (element) {
+  return new bootstrap.Alert(element)
+})
