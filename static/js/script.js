0fd30c444495d22072fa5369eefe4d96ad80a2fc
KingaSzmyd <kingaszmyd@gmail.com>
2021-10-10 14:58:34 +0000

    Adding funcionality to upload button

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,3 @@
-var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
-var collapseList = collapseElementList.map(function (collapseEl) {
-  return new bootstrap.Collapse(collapseEl)
-})
-
-
 var alertList = document.querySelectorAll('.alert')
 var alerts =  [].slice.call(alertList).map(function (element) {
   return new bootstrap.Alert(element)
